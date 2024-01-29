a1b9cc1c97bf838f4ecddac57b5226fc01af0c4f
Andrey Smirnov <gnecro@yandex.ru>
2024-01-29 16:41:31 +0300

    Знакомство с языком Python. Домашняя работа к 3-му семминару

---
+++ python_intro/sem3/task1.py
@@ -0,0 +1,10 @@
+'''
+Требуется вычислить, сколько раз встречается некоторое число k в массиве list_1.
+
+Найдите количество и выведите его.
+'''
+# from random import randint
+# list_1 = [randint(1, 10) for el in range(randint(10, 20))]
+# k = randint(1, 10)
+# print(list_1, k)
+print(list_1.count(k))
